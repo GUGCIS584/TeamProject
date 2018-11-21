ed10472311c9bd9c23d67b80134d9566140767fb
alotaibi028 <42802405+alotaibi028@users.noreply.github.com>
2018-11-21 12:35:57 -0800

    edit

--- src/Database/project-team-system.sql
+++ src/Database/project-team-system.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2018 at 05:19 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Generation Time: Nov 21, 2018 at 09:34 PM
+-- Server version: 5.7.21-log
+-- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +17,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `studentdetails`
+-- Database: `project-team-system`
 --
+CREATE DATABASE IF NOT EXISTS `project-team-system` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `project-team-system`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facultydetails`
+--
+
+DROP TABLE IF EXISTS `facultydetails`;
+CREATE TABLE `facultydetails` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `typeofjob` varchar(255) NOT NULL,
+  `subject_total` enum('1','2','3','4','5') NOT NULL,
+  `subject_name1` varchar(255) NOT NULL,
+  `subject_name2` varchar(255) NOT NULL,
+  `subject_name3` varchar(255) NOT NULL,
+  `subject_name4` varchar(255) NOT NULL,
+  `subject_name5` varchar(255) NOT NULL,
+  `yearly_salary` varchar(255) NOT NULL,
+  `pay_per_subject` varchar(255) NOT NULL,
+  `hours` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -28,6 +51,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `studentdetails`
 --
 
+DROP TABLE IF EXISTS `studentdetails`;
 CREATE TABLE `studentdetails` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -46,6 +70,12 @@ CREATE TABLE `studentdetails` (
 --
 
 --
+-- Indexes for table `facultydetails`
+--
+ALTER TABLE `facultydetails`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `studentdetails`
 --
 ALTER TABLE `studentdetails`
@@ -56,12 +86,15 @@ ALTER TABLE `studentdetails`
 --
 
 --
+-- AUTO_INCREMENT for table `facultydetails`
+--
+ALTER TABLE `facultydetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `studentdetails`
 --
 ALTER TABLE `studentdetails`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
