59aeec800b0428dbbc7a7b9afca62362017c2cfa
patel199 <patel199@knights.gannon.edu>
2018-11-27 11:32:53 -0500

    edited

--- src/index.php
+++ src/index.php
@@ -1,208 +1,41 @@
 <?php
 
+
+
 ?>
 
 
 <!DOCTYPE html>
 <head>
-	<title>University Financial System</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
-
-
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-
-<script>
-$(document).ready(function(){
-
-    $("#btnsubmit").click(function(){
-
-		if($('#name').val()=="" || $('#department').val()=="" || $('#major').val()=="" || $('#noc').val()=="" || $('#fpc').val()=="" || $('#ws').val()=="" || $('#noh').val()=="" || $('#tow').val()=="" || $('#pph').val()==""){
-			alert("Fill all Fields First!")
-			}
-		else{
-
-        var edata = $("#frmdata").serialize();
-			$.ajax({
-		url:"submitapi.php",
-		type:"POST",
-		data:edata,
-		beforeSend: function()
-		{
-			$("#btnsubmit").html("<center><i class='fa fa-spinner fa-spin'></center>");
-		},
-		success: function(res)
-		{
-		$("#btnsubmit").html('<span>Submit<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i></span>');
-		$("#frmdata").hide(500);
-		$("#thanks").html(res);
-		$("#thanks").show(500);
-
-		}
-
-		});
-		}
-    });
-});
-</script>
+    <title>University Financial System</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
-<body style="background:#e6e6e6">
-
-<div class="container">
-  <span class="contact100-form-title">
-    University Financial System
-  </span>
-            <nav class="navbar navbar-expand-lg navbar-expand-sm navbar-light bg-light" style="border-radius:10px">
+</head>
+<body>
+    <h1 style="color: blue; text-align: center">University Financial System</h1>
+    <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                          <a class="nav-link label-input100" href="/src/index.php">Home <span class="sr-only">(current)</span></a>
+                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link label-input100" href="/src/student/StIndex.php">Student</a>
+                          <a class="nav-link" href="/src/student/StIndex.php">Student</a>
                         </li>
-												<li class="nav-item">
-													<?php
-													$con = mysqli_connect('localhost', 'root', 'root', 'project-team-system');
-													$sql= $con->query("SELECT id,COUNT(*) FROM studentdetails");
-
-													while($data = mysqli_fetch_array($sql)){
-													echo '<input type="text" name="student" id="student" style="width:30px"class="input100" disabled value='.$data['COUNT(*)'].' />';
-													}
-													?>
-												 </li>
-												<li class="nav-item">
-													  <span class="sr-only">(</span><label type="text" name="student" id="student"><span class="sr-only">)</span>
- 												 </li>
-												<li class="nav-item">
-                                <a class="nav-link label-input100" href="/src/Faculty/Faculty.php">Faculty</a>
+                        <li class="nav-item">
+                                <a class="nav-link" href="/src/Faculty/Faculty.php">Faculty</a>
                               </li>
-															<li class="nav-item">
-																<?php
-																$con = mysqli_connect('localhost', 'root', 'root', 'project-team-system');
-															  $sql= $con->query("SELECT id,COUNT(*) FROM facultydetails");
-
-																while($data = mysqli_fetch_array($sql)){
-																echo '<input type="text" name="faculty" id="faculty" style="width:30px"class="input100" disabled value='.$data['COUNT(*)'].' />';
-																}
-																?>
-															 </li>
-
                         <li class="nav-item">
-                                <a class="nav-link label-input100" href="/src/supplier/supplier.php">Supplier</a>
+                                <a class="nav-link" href="/src/supplier/supplier.php">Supplier</a>
                                   </li>
-																	<li class="nav-item">
-																		<?php
-																		$con = mysqli_connect('localhost', 'root', 'root', 'project-team-system');
-																	  $sql= $con->query("SELECT supp_id,COUNT(*) FROM Supplier");
-
-																		while($data = mysqli_fetch_array($sql)){
-																		echo '<input type="text" name="supplier" id="supplier" style="width:30px"class="input100" disabled value='.$data['COUNT(*)'].' />';
-																		}
-																		?>
-																	 </li>
-
 
                       </ul>
-
-
                     </div>
                   </nav>
-<br/>
-									<form  method="post" class="contact100-form validate-form" id="frmdata">
 
-									<table class="col-lg-12 col-md-12 col-sm-12 " >
-
-										<tr>
-											<td>
-												<div class="wrap-input100 validate-input bg1" style="width:98%">
-											    <span class="label-input100">Monthly Student Expenses*</span>
-													<?php
-													$con = mysqli_connect('localhost', 'root', 'root', 'project-team-system');
-													$sql= $con->query("SELECT SUM(noh*pph*4) FROM studentdetails");
-
-													while($data = mysqli_fetch_array($sql)){
-													echo '<input class="input100" type="text" name="student_expense" id="student_expense" placeholder="Student Expenses" disabled value='.'$&nbsp;'.$data['SUM(noh*pph*4)'].' />';
-													}
-													?>
-												 </div>
-											</td>
-											<td>
-												<div class="wrap-input100 validate-input bg1" style="margin-left:auto; width:98%">
-											    <span class="label-input100">Monthly Student Income*</span>
-													<?php
-													$con = mysqli_connect('localhost', 'root', 'root', 'project-team-system');
-													$sql= $con->query("SELECT SUM(noc*fpc/6) FROM studentdetails");
-
-													while($data = mysqli_fetch_array($sql)){
-													echo '<input class="input100" type="text" name="student_income" id="student_income" placeholder="Student Income" disabled value='.'$&nbsp;'.$data['SUM(noc*fpc/6)'].' />';
-													}
-													?>
-
-											  </div>
-											</td>
-										</tr>
-
-										<tr>
-											<td>
-												<div class="wrap-input100 validate-input bg1"style="width:98%">
-													<span class="label-input100">Monthly Faculty Expenses*</span>
-													<?php
-													$con = mysqli_connect('localhost', 'root', 'root', 'project-team-system');
-													$sql= $con->query("SELECT SUM(faculty_total) FROM facultydetails");
-
-													while($data = mysqli_fetch_array($sql)){
-													echo '<input class="input100" type="text" name="faculty_expense" id="faculty_expense" placeholder="Faculty Expense" disabled value='.'$&nbsp;'.$data['SUM(faculty_total)'].' />';
-													}
-													?>
-													</div>
-											</td>
-											<td>
-												<div class="wrap-input100 validate-input bg1" style="margin-left:auto; width:98%">
-													<span class="label-input100">Monthly University Expenses*</span>
-													<input class="input100" type="text" name="university_expense" id="university_expense" placeholder="University Expense" disabled value='$&nbsp;'/>
- 						 					</div>
-											</td>
-										</tr>
-										<tr>
-
-											<td>
-												<div class="wrap-input100 validate-input bg1"style="width:98%">
-													<span class="label-input100">Monthly Supplier Expenses*</span>
-													<?php
-													$con = mysqli_connect('localhost', 'root', 'root', 'project-team-system');
-													$sql= $con->query("SELECT SUM(total) FROM supplier");
-
-													while($data = mysqli_fetch_array($sql)){
-													echo '<input class="input100" type="text" name="supplier_expense" id="supplier_expense" placeholder="Supplier Expenses" disabled value='.'$&nbsp;'.$data['SUM(total)'].' />';
-													}
-													?>		 												</div>
-											</td>
-
-											<td>
-												<div class="wrap-input100 validate-input bg1" style="margin-left:auto; width:98%">
-											    <span class="label-input100">Monthly University Income*</span>
-											    <input class="input100" type="text" name="university_income" id="university_income" placeholder="University Income" disabled value='$&nbsp;'/>
-											  </div>
-											</td>
-										</tr>
-
-
-									</table>
-								</form>
     </div>
 
 
--- src/supplier/supp-submit.php
+++ src/supplier/supp-submit.php
@@ -4,17 +4,25 @@ include 'config.php';
 
 
     $name = mysqli_real_escape_string($con,$_POST["Company Name"]);
-	
-   
+    $product = mysqli_real_escape_string($con,$_POST["Product"]);
+  	$price = mysqli_real_escape_string($con,$_POST["Price"]);
+  	$quantities = mysqli_real_escape_string($con,$_POST["Quantities"]);
+  	$date = mysqli_real_escape_string($con,$_POST["Date"]);
+  	$total = mysqli_real_escape_string($con,$_POST["Total"]);
+  	$totalorder = mysqli_real_escape_string($con,$_POST["Total Order"]);
 
-	
-		$q = mysqli_query($con,"INSERT INTO `atom`(`name`) VALUES ('".$name."')");
-		
-		
-	
+
+
+
+    $query="INSERT INTO facultydetails (name,product,price,qunatities,date,total,total order)
+    VALUES ('$name','$product','$price','$quantities','$date','$total','$totalorder')";
+
+
+
+
 
 	if($q>0){
-			  echo 'Student Details has been added!';
+			  echo 'Supplier Details has been added!';
 
 
   }
